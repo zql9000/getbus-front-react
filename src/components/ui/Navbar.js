d6d40645814c5e3653a38e30c0f203c87e1b533b
Ezequiel López <lelopez@indracompany.com>
2021-03-28 19:12:25 -0300

    Changes in navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { startLogout } from '../../actions/authActions';
+import './navbar.css';
 
 export const Navbar = () => {
   const { name, lastName, role } = useSelector((state) => state.auth);
@@ -12,37 +13,36 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          <img alt="Get Bus" src="GetBus.png" height="30" />
-        </Link>
+    <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <Link className="navbar-brand col-md-3 col-lg-2 me-0 px-3" to="/">
+        <img alt="Get Bus" src="GetBus.png" height="30" />
+      </Link>
+      <div className="navbar-end">
+        <div className="small user-detail">
+          <span>
+            {name} {lastName}
+          </span>
+          <p className="m-0">{role}</p>
+        </div>
+        <ul className="navbar-nav px-3 navbar-center">
+          <li className="nav-item text-nowrap">
+            <span className="nav-link pointer" onClick={handleLogout}>
+              <i class="fas fa-sign-out-alt"></i>
+            </span>
+          </li>
+        </ul>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler text-end d-md-none collapsed me-2"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
-          aria-controls="navbarText"
+          data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <div className="navbar-nav me-auto"></div>
-          <div className="navbar-text pe-2 small">
-            <span>
-              {name} {lastName}
-            </span>
-            <br />
-            <span>{role}</span>
-          </div>
-          <button className="btn btn-outline-danger" onClick={handleLogout}>
-            <i className="fas fa-sign-out-alt"></i>
-            <span> Salir</span>
-          </button>
-        </div>
       </div>
-    </nav>
+    </header>
   );
 };
