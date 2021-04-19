1cd5a2fb224c4003b3efe744418ec1b8ca3904c7
Ezequiel López <lelopez@indracompany.com>
2021-04-18 23:22:59 -0300

    Fix scroll sidebar and rename menu item

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -9,7 +9,7 @@ export const Sidebar = () => {
       className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
     >
       <div className="position-sticky pt-3">
-        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-2 mb-1 text-muted">
           <span>Ventas</span>
         </h6>
         <ul className="nav flex-column mb-2">
@@ -113,9 +113,9 @@ export const Sidebar = () => {
               activeClassName="active"
               className="nav-link"
               exact
-              to="/tramos"
+              to="/recorridos"
             >
-              <i className="fas fa-route"></i> Tramos
+              <i className="fas fa-route"></i> Recorridos
             </NavLink>
           </li>
           <li className="nav-item">
